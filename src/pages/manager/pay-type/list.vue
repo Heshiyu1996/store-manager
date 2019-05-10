7f91203b2b402c5ebefb6088e23676040d468673
heshiyu <450979309@qq.com>
2019-05-10 11:52:02 +0800

    feature: pay-type.md

--- src/pages/manager/pay-type/list.vue
+++ src/pages/manager/pay-type/list.vue
@@ -3,139 +3,124 @@
         <div class="top-wrapper">
             <u-layout>
                 <el-select v-model="searchParams.storeId" filterable placeholder="请选择门店">
-                    <el-option v-for="item in storeList" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+                    <el-option v-for="item in storeListModified" :key="item.id" :label="item.name" :value="item.id"> </el-option>
                 </el-select>
-                <el-select v-model="searchParams.useStatus" placeholder="请选择使用状态">
-                    <el-option v-for="item in useStatusList" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+                <el-select v-model="searchParams.status" placeholder="请选择使用状态">
+                    <el-option v-for="item in STATUS_LIST" :key="item.value" :label="item.label" :value="item.value"> </el-option>
                 </el-select>
-                <u-input v-model.trim="searchParams.searchContent" maxLength="100" placeholder="请输入支付类型" searchIcon @key-press-enter="goSearch" />
+                <u-input v-model.trim="searchParams.name" maxLength="100" placeholder="请输入支付类型" searchIcon @key-press-enter="_getList" />
             </u-layout>
             <u-layout class="operation">
                 <el-tooltip class="item" effect="dark" content="添加" placement="top">
-                    <el-button type="primary" icon="el-icon-plus" circle></el-button>
+                    <el-button type="primary" icon="el-icon-plus" circle @click="addPatch"></el-button>
                 </el-tooltip>
                 <el-tooltip class="item" effect="dark" content="删除" placement="top">
-                    <el-button type="warning" icon="el-icon-minus" circle></el-button>
+                    <el-button type="warning" icon="el-icon-minus" circle @click="updateRows(OPERATION_TYPE.DELETE)"></el-button>
                 </el-tooltip>
                 <el-tooltip class="item" effect="dark" content="启用" placement="top">
-                    <el-button type="success" icon="el-icon-check" circle></el-button>
+                    <el-button type="success" icon="el-icon-check" circle @click="updateRows(OPERATION_TYPE.OPEN)"></el-button>
                 </el-tooltip>
                 <el-tooltip class="item" effect="dark" content="禁用" placement="top">
-                    <el-button type="danger" icon="el-icon-close" circle></el-button>
+                    <el-button type="danger" icon="el-icon-close" circle @click="updateRows(OPERATION_TYPE.CLOSE)"></el-button>
                 </el-tooltip>
             </u-layout>
         </div>
 
         <u-layout class="content-wrapper" direction="v">
-            <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
-                <el-table-column type="selection" width="55"> </el-table-column>
-                <el-table-column prop="payTypeName" label="支付类型名称" width="180"> </el-table-column>
-                <el-table-column prop="payTypeGroupName" label="支付类型分组" width="180"> </el-table-column>
-                <el-table-column prop="payTypeNum" label="支付类型编码"> </el-table-column>
-                <el-table-column prop="settlementPrice" label="结算价"> </el-table-column>
-                <el-table-column prop="orderNum" label="排序号" width="70"> </el-table-column>
-                <el-table-column prop="storeName" label="所属门店" width="200" show-overflow-tooltip> </el-table-column>
-                <el-table-column prop="useStatus" label="使用状态" width="80">
-                    <template slot-scope="{ row }">
-                        <el-switch v-model="row.useStatus" @change="switchRow(row)" active-color="#13ce66" inactive-color="#ff4949"> </el-switch>
-                    </template>
-                </el-table-column>
-                <el-table-column label="操作" width="120">
-                    <template slot-scope="{ row }">
+            <u-table ref="operationTable" :list="list" auto is-list>
+                <template slot-scope="{ row }">
+                    <u-table-column width="2vw" label="" ellipse><el-checkbox v-model="row.checked"/></u-table-column>
+                    <u-table-column width="12vw" label="支付类型名称" ellipse>{{ row.name || '-' }}</u-table-column>
+                    <u-table-column width="12vw" label="支付类型分组" ellipse>{{ row.groupId || '-' }}</u-table-column>
+                    <u-table-column width="12vw" label="结算价" ellipse>{{ row.prize }}</u-table-column>
+                    <u-table-column width="12vw" label="所属门店" ellipse> {{ row.storeId }} </u-table-column>
+                    <u-table-column width="12vw" label="使用状态" ellipse>
+                        <el-switch v-model="row.status" @change="switchRow(row)" active-color="#13ce66" inactive-color="#ff4949"> </el-switch>
+                    </u-table-column>
+                    <u-table-column label="操作">
                         <u-layout direction="h">
-                            <i class="icon el-icon-edit" @click="editRow(row)"></i> <i class="icon el-icon-delete" @click="deleteRow(row)"></i>
+                            <i class="icon el-icon-edit" @click="editRow(row)"></i> <i class="icon el-icon-delete" @click="deleteRow(row.id)"></i>
                         </u-layout>
-                    </template>
-                </el-table-column>
-            </el-table>
+                    </u-table-column>
+                </template>
+            </u-table>
 
             <el-pagination
+                v-show="list.length"
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
                 :current-page="searchParams.currentPage"
                 :page-sizes="[10, 50, 100]"
                 :page-size="searchParams.pageSize"
                 layout="total, sizes, prev, pager, next, jumper"
-                :total="tableData.length"
+                :total="list.length"
             >
             </el-pagination>
         </u-layout>
+
+        <AUserinfoModal :visible="isOpenUserInfoModal" @close="closeUserInfoModal" />
     </u-layout>
 </template>
 
 <script>
+import AUserinfoModal from '@/components/account/a-userinfo-modal'
+import { USER_TYPE_MAP, MODIFY_MODAL_TYPE, CARD_TYPE_MAP } from '@/utils/config'
+import { getPaymentList, deletePayment, updatePaymentStatus } from '@/server/api'
+import { createNamespacedHelpers } from 'vuex'
+
+const { mapGetters } = createNamespacedHelpers('login')
+const OPERATION_TYPE = {
+    DELETE: 1,
+    OPEN: 2,
+    CLOSE: 3
+}
+const STATUS_LIST = [
+    {
+        value: '',
+        label: '所有状态'
+    },
+    {
+        value: true,
+        label: '启用'
+    },
+    {
+        value: false,
+        label: '禁用'
+    }
+]
+
 export default {
     name: 'pay-list',
+    components: { AUserinfoModal },
     data() {
         return {
             searchParams: {
-                storeId: '1',
-                useStatus: '1',
-                searchContent: '',
+                storeId: '',
+                status: '', // 不传则启用、禁用都查。false:查禁用；true:查启用
+                name: '',
+                groupId: '',
+
                 currentPage: 1,
                 pageSize: 50
             },
 
-            useStatusList: [
-                {
-                    value: '1',
-                    label: '所有状态'
-                },
-                {
-                    value: '2',
-                    label: '启用'
-                },
-                {
-                    value: '3',
-                    label: '禁用'
-                }
-            ],
-            storeList: [
-                {
-                    value: '1',
-                    label: '大笨象密室深圳一店'
-                },
-                {
-                    value: '2',
-                    label: '大笨象密室深圳二店'
-                },
-                {
-                    value: '3',
-                    label: '大笨象密室深圳三店'
-                }
-            ],
+            list: [],
 
-            tableData: [
-                {
-                    payTypeId: 1,
-                    payTypeName: '补差价哆啦宝',
-                    payTypeNum: '222223',
-                    payTypeGroupName: '支付类型分组1',
-                    settlementPrice: 1.0,
-                    orderNum: 5,
-                    storeName: '大笨象密室深圳一店',
-                    storeId: 1,
-                    useStatus: false
-                },
-                {
-                    payTypeId: 2,
-                    payTypeName: '补差价现金',
-                    payTypeNum: '888',
-                    payTypeGroupName: '支付类型分组2',
-                    settlementPrice: 2.0,
-                    orderNum: 2,
-                    storeName: '大笨象密室深圳一店',
-                    storeId: 1,
-                    useStatus: true
-                }
-            ]
+            isOpenUserInfoModal: false,
+
+            USER_TYPE_MAP: [{ label: '所有用户', value: '' }].concat(USER_TYPE_MAP),
+            OPERATION_TYPE,
+            STATUS_LIST
         }
     },
-    watch: {
-        'searchParams.storeId'() {
-            this._getList(true)
+    computed: {
+        storeListModified() {
+            return [{ id: '', name: '所有门店' }].concat(this.getStoreListStore)
         },
-        'searchParams.useStatus'() {
+        ...mapGetters(['getUserInfoStore', 'getStoreListStore'])
+    },
+    watch: {
+        'searchParams.userType'() {
             this._getList(false)
         },
         'searchParams.currentPage'() {
@@ -143,33 +128,85 @@ export default {
         },
         'searchParams.pageSize'() {
             this._getList(true)
+        },
+        'searchParams.storeId'() {
+            this._getList(true)
+        },
+        'searchParams.status'() {
+            this._getList(true)
         }
     },
+    created() {
+        this._getList(true)
+    },
     methods: {
+        addPatch() {
+            this.$bus.$emit('open-userinfo-modal', {}, MODIFY_MODAL_TYPE.ADD)
+            this.isOpenUserInfoModal = true
+        },
+        updateRows(otype) {
+            let params = this._fetchSelectedRows()
+            switch (otype) {
+                case OPERATION_TYPE.DELETE:
+                    console.log('delete', params)
+                    this.deleteRow(...params)
+                    break
+                case OPERATION_TYPE.OPEN:
+                case OPERATION_TYPE.CLOSE:
+                    console.log('close', params)
+                    var isOpen = otype === OPERATION_TYPE.OPEN
+                    updatePaymentStatus({ ids: params, status: isOpen }).then(() => {
+                        this.$message(`${isOpen ? '启用' : '删除'} 成功`)
+                        this._getList(true)
+                    })
+                    break
+            }
+        },
+        _fetchSelectedRows() {
+            debugger
+
+            let delList = this.list.filter(item => item.checked).map(item => item.id)
+            console.log(delList)
+            return delList
+        },
         switchRow(row) {
             console.log(row)
         },
         editRow(row) {
             console.log(row)
+
+            this.$bus.$emit('open-userinfo-modal', row, MODIFY_MODAL_TYPE.EDIT)
+            this.isOpenUserInfoModal = true
         },
-        deleteRow(row) {
-            console.log(row)
-        },
-        goSearch() {
-            console.log(123)
+        deleteRow(...ids) {
+            this.$confirm(`是否删除所选支付类型 ？`).then(() =>
+                deletePayment(ids).then(() => {
+                    this.$message('删除成功')
+                    this._getList(false)
+                })
+            )
         },
         _getList(isNew) {
             console.log('拉取列表，isNew：', isNew)
-            // this.searchKeyword = this.searchParams.searchContent
-            // isNew && (this.searchParams.currentPage = 1)
-            // sortType && (this.searchParams.sortType = sortType)
-            // order && (this.searchParams.order = order)
+            isNew && (this.searchParams.currentPage = 1)
 
-            // getList(this.searchParams).then(data => {
-            //     this.intentionList = data.dialogueList || []
-            //     this.totalCount = data.totalCount || 0
-            //     this._combindNames()
-            // })
+            getPaymentList(this.searchParams).then(data => {
+                this.list = data.list || []
+                this.list.forEach(item => this.$set(item, 'checked', false))
+                this.totalCount = data.totalCount || 0
+            })
+        },
+        _findUserType(utype) {
+            if (typeof utype === 'undefined') return '-'
+            return this.USER_TYPE_MAP.find(item => utype === item.value).label
+        },
+        _findCardType(ctype) {
+            if (ctype === null || typeof ctype === 'undefined') return '-'
+            return CARD_TYPE_MAP.find(item => ctype === item.value).label
+        },
+        _hasRight(type) {
+            if (typeof type === 'undefined') return false
+            return this.getUserInfoStore.userType > type
         },
         // 多选
         handleSelectionChange(val) {
@@ -185,6 +222,10 @@ export default {
         handleCurrentChange(val) {
             this.searchParams.currentPage = val
             console.log(`当前页: ${val}`)
+        },
+        closeUserInfoModal() {
+            this.isOpenUserInfoModal = false
+            this._getList(false)
         }
     }
 }
