b954d8b2bd2ded88790269a363cfd968fac3d891
heshiyu <450979309@qq.com>
2019-05-12 17:10:39 +0800

    feature: remove vuelidate in userinfo-modal

--- src/components/account/a-userinfo-modal.vue
+++ src/components/account/a-userinfo-modal.vue
@@ -1,45 +1,34 @@
 <template>
-    <u-modal
-        :visible="visible"
-        :enableConfirm="$v.$anyDirty && !$v.$invalid"
-        title="用户修改信息"
-        @before-close="submit"
-        @close="closeModal"
-        class="a-password-modal"
-    >
+    <u-modal :visible="visible" title="用户修改信息" @before-close="submit" @close="closeModal" class="a-password-modal">
         <el-form ref="form" :model="form" label-width="68px">
             <el-form-item v-if="isGM" label="账号类型">
-                <el-select v-model="$v.form.userType.$model" filterable placeholder="请选择账号类型" @change="changeUserType">
+                <el-select v-model="form.userType" filterable placeholder="请选择账号类型" @change="changeUserType">
                     <el-option v-for="item in userTypeModifiedList" :key="item.value" :label="item.label" :value="item.value"> </el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="姓名">
-                <u-input v-model="$v.form.realName.$model" placeholder="请输入姓名" />
-                <u-error :visible="!$v.form.realName.required" text="请输入姓名" />
-                <u-error :visible="!$v.form.realName.minLength" text="姓名不能少于2个字符" />
+                <u-input v-model="form.realName" placeholder="请输入姓名" />
             </el-form-item>
             <el-form-item label="手机">
-                <u-input v-model="$v.form.phone.$model" :regex="/^[0-9\u4e00-\u9fa5]+$/g" maxLength="11" placeholder="请输入手机号" />
-                <u-error :visible="!$v.form.phone.required" text="手机必填" />
-                <u-error :visible="!$v.form.phone.isPhone" text="手机格式不正确" />
+                <u-input v-model="form.phone" :regex="/^[0-9\u4e00-\u9fa5]+$/g" maxLength="11" placeholder="请输入手机号" />
             </el-form-item>
             <el-form-item label="生日">
-                <el-date-picker v-model="$v.form.birthday.$model" type="date" placeholder="请输入你的生日" size="medium"> </el-date-picker>
+                <el-date-picker v-model="form.birthday" type="date" placeholder="请输入你的生日" size="medium"> </el-date-picker>
             </el-form-item>
             <template v-if="form.userType === USER_TYPE.NORMAL">
                 <el-form-item label="卡号">
-                    <u-input v-model="$v.form.cardId.$model" placeholder="请输入卡号" />
+                    <u-input v-model="form.cardId" placeholder="请输入卡号" />
                 </el-form-item>
                 <el-form-item label="卡种">
-                    <el-select v-model="$v.form.cardType.$model" filterable placeholder="请选择卡种">
+                    <el-select v-model="form.cardType" filterable placeholder="请选择卡种">
                         <el-option v-for="item in CARD_TYPE_MAP" :key="item.value" :label="item.label" :value="item.value"> </el-option>
                     </el-select>
                 </el-form-item>
             </template>
             <template v-else>
                 <el-form-item label="所属门店">
-                    <el-select v-model="$v.form.storeId.$model" filterable placeholder="请选择所属门店">
-                        <el-option v-for="item in userTypeModifiedList" :key="item.value" :label="item.label" :value="item.value"> </el-option>
+                    <el-select v-model="form.storeId" filterable placeholder="请选择所属门店">
+                        <el-option v-for="item in getStoreListStore" :key="item.id" :label="item.name" :value="item.id"> </el-option>
                     </el-select>
                 </el-form-item>
             </template>
@@ -49,7 +38,7 @@
 
 <script>
 import { CloseModalMixin, InvalidCheckMixin } from '@/components/common/mixins'
-import { required, requiredIf, minLength, helpers } from 'vuelidate/lib/validators'
+// import { required, requiredIf, minLength, helpers } from 'vuelidate/lib/validators'
 import { setUserInfo } from '@/server/api'
 import { USER_TYPE, USER_TYPE_MAP, MODIFY_MODAL_TYPE, CARD_TYPE_MAP } from '@/utils/config'
 import { createNamespacedHelpers } from 'vuex'
@@ -84,15 +73,6 @@ export default {
             CARD_TYPE_MAP
         }
     },
-    watch: {
-        visible(val) {
-            if (val) {
-                this.$v.$reset()
-            } else {
-                this._resetData()
-            }
-        }
-    },
     computed: {
         isGM() {
             return this.getUserInfoStore.userType > USER_TYPE.NORMAL
@@ -100,7 +80,7 @@ export default {
         userTypeModifiedList() {
             return USER_TYPE_MAP.filter(item => item.value <= this.getUserInfoStore.userType)
         },
-        ...mapGetters(['getUserInfoStore'])
+        ...mapGetters(['getUserInfoStore', 'getStoreListStore'])
     },
     created() {
         this.$bus.$on('open-userinfo-modal', accountDetail => {
@@ -111,54 +91,51 @@ export default {
     destroyed() {
         this.$bus.$off('open-userinfo-modal')
     },
-    validations: {
-        form: {
-            userType: { required },
-            realName: {
-                required,
-                minLength: minLength(2)
-            },
-            phone: {
-                required,
-                isPhone(value) {
-                    let regex_phone = /^1[345678][0-9]{9}$/
-                    return !helpers.req(value) || regex_phone.test(value) // 有值时!helpers.req为false，这时候才采取后面校验；没值时，不校验
-                }
-            },
-            birthday: { required },
-            cardId: {
-                required: requiredIf(function() {
-                    console.log(this.form.userType)
-                    return this.form.userType === USER_TYPE.NORMAL
-                })
-            },
-            cardType: {
-                required: requiredIf(function() {
-                    return this.form.userType === USER_TYPE.NORMAL
-                })
-            },
-            storeId: {
-                required: requiredIf(function() {
-                    return this.form.userType !== USER_TYPE.NORMAL
-                })
-            }
-        }
-    },
+    // validations: {
+    //     form: {
+    //         userType: { required },
+    //         realName: {
+    //             required,
+    //             minLength: minLength(2)
+    //         },
+    //         phone: {
+    //             required,
+    //             isPhone(value) {
+    //                 let regex_phone = /^1[345678][0-9]{9}$/
+    //                 return !helpers.req(value) || regex_phone.test(value) // 有值时!helpers.req为false，这时候才采取后面校验；没值时，不校验
+    //             }
+    //         },
+    //         birthday: { required },
+    //         cardId: {
+    //             required: requiredIf(function() {
+    //                 console.log(this.form.userType)
+    //                 return this.form.userType === USER_TYPE.NORMAL
+    //             })
+    //         },
+    //         cardType: {
+    //             required: requiredIf(function() {
+    //                 return this.form.userType === USER_TYPE.NORMAL
+    //             })
+    //         },
+    //         storeId: {
+    //             required: requiredIf(function() {
+    //                 return this.form.userType !== USER_TYPE.NORMAL
+    //             })
+    //         }
+    //     }
+    // },
     methods: {
         submit(e) {
             // 检查数据
-            if (this._isInvalid()) return
+            // if (this._isInvalid()) return
 
             e.preventDefault()
             if (!e.ok) {
-                this._openConfirmWhenChangedData()
+                this.closeModal()
                 return
             }
 
             this._setUserInfo()
-        },
-        _openConfirmWhenChangedData() {
-            this.$v.$anyDirty ? this.$confirm('确认放弃修改吗？').then(() => this.closeModal(true)) : this.closeModal(false)
         },
         _setUserInfo() {
             if (typeof this.form.birthday !== 'number') {
