6dd5abc3823db75a0782eb3f689084a7bf1eeed5
heshiyu <450979309@qq.com>
2019-05-19 17:33:59 +0800

    feature: item list 10%

--- src/server/api/materialAPI.js
+++ src/server/api/materialAPI.js
@@ -42,3 +42,43 @@ export const editMaterialHouse = data =>
         method: 'put',
         data
     })
+
+// 获取库存列表
+export const getMaterialItemList = data =>
+    http({
+        url: `/api/material/item/list`,
+        method: 'post',
+        data
+    })
+
+// 新建库存
+export const addMaterialItem = data =>
+    http({
+        url: `/api/material/item/add`,
+        method: 'post',
+        data
+    })
+
+// 批量禁用/启用（库存）
+export const updateMaterialItemStatus = data =>
+    http({
+        url: `/api/material/item/status`,
+        method: 'put',
+        data
+    })
+
+// 删除库存
+export const deleteMaterialItem = data =>
+    http({
+        url: `/api/material/item/:itemId`,
+        method: 'delete',
+        data
+    })
+
+// 修改仓库信息
+export const editMaterialItem = data =>
+    http({
+        url: `/api/material/item/${data.id}`,
+        method: 'put',
+        data
+    })
